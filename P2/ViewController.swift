baf5a7e3da4582b2c5b0861e5c8da2524883c4cb
Alex Huang <Alex@wifi-544.cs.wisc.edu>
2015-10-02 14:45:58 -0500

    Initial Commit

---
+++ P2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  P2
+//
+//  Created by Alex Huang on 10/2/15.
+//  Copyright © 2015 Alex Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
